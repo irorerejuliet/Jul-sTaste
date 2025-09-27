48ea11c1ebcc6a1c1ea25956f816db934deb2905
irorere juliet <irorerejuliet@gmail.com>
2025-09-27 21:58:46 +0100

    Making changes on the fetched data

--- src/Components/RecipeCard.jsx
+++ src/Components/RecipeCard.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 const RecipeCard = ({recipe}) => {
+  const { label, cuisineType, mealType, uri, image } = recipe?.recipe;
+  const id = uri?.split("#")[1];
 
-  const { image, label, cuisineType, mealType, uri } = recipe?.recipe;
-  const id = uri?.split("#")[1]
+  // // Prefer LARGE image > REGULAR > fallback to image
+  // const largeImage =
+  //   images?.LARGE?.url || images?.REGULAR?.url || recipe?.recipe.image;
 
   return (
-    <Link to={`/recipes/${id}`} className='w-full '>
+    <Link to={`/recipes/${id}`} className="w-full ">
       <div className="bg-gradient-to-t from-black to-transparent shadow w-full rounded-lg">
-        <img src={image}alt={label} className='rounded-lg  w-full'/>
+        <img src={image} alt={label} className="rounded-lg  w-full" />
       </div>
     </Link>
   );
