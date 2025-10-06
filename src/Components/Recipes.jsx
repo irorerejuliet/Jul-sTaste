64df69cd231176d4d5039e143b65d10208c54b7f
irorere juliet <irorerejuliet@gmail.com>
2025-10-06 18:49:09 -0400

    reciepes

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -66,35 +66,37 @@ const Recipes = () => {
     <div className="w-full">
       <div className="w-full flex items-center justify-center pt-10 pb-5 px-0 md:px-10">
         <form className="w-full lg:w-2/4" onSubmit={handleSearchRecipe}>
-        <SearchBar placeholder="eg. Cake, Vegan, Chicken"
-        handleInputChange={handleChange}
-        rightIcon={<BiSearchAlt2 className="text-gray-500"/>}
-        />
+          <SearchBar
+            placeholder="eg. Cake, Vegan, Chicken"
+            handleInputChange={handleChange}
+            rightIcon={<BiSearchAlt2 className="text-gray-500" />}
+          />
         </form>
-
       </div>
-      {
-        recipes?.length > 0 ? (
-          <>
-         
-          {
-            recipes?.map((item, id) =>(
-              <RecipeCard recipe={item} key={id}/>
-            ))
-          }
+      {recipes?.length > 0 ? (
+        <>
+          <div className="w-full flex flex-wrap gap-10 px-0 lg:px-10 py-10">
+            {recipes?.map((item, id) => (
+              <RecipeCard recipe={item} key={id} />
+            ))}
 
-          <div className="flex justify-between items-center py-10">
-            <button className="bg-green-800 text-white px-3 py-1 rounded-full text-sm" onClick={showMore}>Show more</button>
+            <div className="flex justify-between items-center py-10">
+              <button
+                className="bg-green-800 text-white px-3 py-1 rounded-full text-sm"
+                onClick={showMore}
+              >
+                Show more
+              </button>
+            </div>
           </div>
-         
-          </>
-          
-        ):<div className=" w-full items-center justify-center py-10 text-white">
+        </>
+      ) : (
+        <div className=" w-full items-center justify-center py-10 text-white">
           <p className="text-center">No Recipe found</p>
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
 
 export default Recipes
