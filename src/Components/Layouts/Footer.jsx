a3505d3aa5a2f47e9541239e0f464132da378fdd
irorere juliet <irorerejuliet@gmail.com>
2025-10-09 18:19:16 -0400

    footer

--- src/Components/Layouts/Footer.jsx
+++ src/Components/Layouts/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="text-white  py-20 ">
       <div className="container mx-auto px-20 lg:px-20 py20 flex flex-col gap-10 md:flex-row justify-between border-t border-slate-800">
-        <div className="flex">
+        <div className="flex pt-20">
           <p className="font-bold text-center">
             <Link to="/">
               <img
@@ -16,7 +16,7 @@ const Footer = () => {
             </Link>
           </p>
         </div>
-        <div>
+        <div className='pt-20'>
           <p>QUICK LINKS</p>
           <div className="flex flex-col text-start mb-4 md:mb-0">
             <Link
@@ -48,6 +48,41 @@ const Footer = () => {
               className="block md:inline-block py-2 hover:text-gray-500"
             >
               Help
+            </Link>
+          </div>
+        </div>
+        <div className='pt-20'>
+          <p>QUICK LINKS</p>
+          <div className="flex flex-col text-start mb-4 md:mb-0">
+            <Link
+              to="/"
+              className="block md:inline-block py-2 hover:text-gray-500"
+            >
+             Terms and Conditions
+            </Link>
+            <Link
+              to="/recipeDetails"
+              className="block md:inline-block py-2 hover:text-gray-500"
+            >
+             LIcense Agreement
+            </Link>
+            <Link
+              to=""
+              className="block md:inline-block py-2 hover:text-gray-500"
+            >
+             Privacy Policy
+            </Link>
+            <Link
+              to=""
+              className="block md:inline-block py-2 hover:text-gray-500"
+            >
+             Copyright Information
+            </Link>
+            <Link
+              to=""
+              className="block md:inline-block py-2 hover:text-gray-500"
+            >
+              Cookies Policy
             </Link>
           </div>
         </div>
