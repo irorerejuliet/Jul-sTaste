92b183789e60036971372954eba0942e3f45bf51
irorere juliet <irorerejuliet@gmail.com>
2025-10-09 17:58:07 -0400

    footer

--- src/Components/Layouts/Footer.jsx
+++ src/Components/Layouts/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="text-white  py-20 ">
       <div className="container mx-auto px-20 lg:px-20 py20 flex flex-col gap-10 md:flex-row justify-between border-t border-slate-800">
         <div className="flex">
-          <p className='font-bold text-center'>
+          <p className="font-bold text-center">
             <Link to="/">
               <img
                 src="/Images/Logoo.png"
@@ -18,8 +18,37 @@ const Footer = () => {
         </div>
         <div>
           <p>QUICK LINKS</p>
-          <div className='flex flex-col text-start mb-4 md'>
-
+          <div className="flex flex-col text-start mb-4 md:mb-0">
+            <Link
+              to="/"
+              className="block md:inline-block py-2 hover:text-gray-500"
+            >
+              Home
+            </Link>
+            <Link
+              to="/recipeDetails"
+              className="block md:inline-block py-2 hover:text-gray-500"
+            >
+              About
+            </Link>
+            <Link
+              to=""
+              className="block md:inline-block py-2 hover:text-gray-500"
+            >
+              Service
+            </Link>
+            <Link
+              to=""
+              className="block md:inline-block py-2 hover:text-gray-500"
+            >
+              Contact
+            </Link>
+            <Link
+              to=""
+              className="block md:inline-block py-2 hover:text-gray-500"
+            >
+              Help
+            </Link>
           </div>
         </div>
       </div>
