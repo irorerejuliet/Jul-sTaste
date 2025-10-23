f4c78a4c127811b024e0869dcdfe2ed34cc696ef
irorere juliet <irorerejuliet@gmail.com>
2025-10-23 17:05:43 +0100

    footer

--- src/utils/index.js
+++ src/utils/index.js
@@ -24,6 +24,6 @@ export async function fetchRecipes({ query, limit }) {
     return [];
   }
 }
-    export async function fetchRecipes(id){
-      const url = `https://dummyjson.com/recipes`;
-    }
+    // export async function fetchRecipes(id){
+    //   const url = `https://dummyjson.com/recipes`;
+    // }
