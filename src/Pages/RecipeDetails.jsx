bb935b4c824a8feda9e2809a55be5eb9943d3ed2
irorere juliet <irorerejuliet@gmail.com>
2025-11-07 04:00:14 +0100

    recipeDetails

--- src/Pages/RecipeDetails.jsx
+++ src/Pages/RecipeDetails.jsx
@@ -1,6 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const RecipeDetails = () => {
+
+  const [recipe, setRecipe] = useState(null)
+  const [recipes, setRecipes] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const {id} = useParams()
+
+  const getRecipe = async (id) => {
+    try {
+      
+      setLoading(true);
+
+      const data = await fetchRecipe(id);
+
+      setRecipe(data);
+
+      const recommend = await fetchRecipes({ query: recipe?.label, limit: 5 });
+
+      setRecipes(recommend);
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+
+      setLoading(false)
+      
+    }
+  }
+
+  useEffect(() => {
+    getRecipe(id)
+  }, [id])
+
+  if(loading){
+    return(
+      <div><Loading></div>
+    )
+  }
+
   return (
     <div>
       
